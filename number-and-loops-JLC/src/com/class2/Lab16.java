2323ee5bc56cdc829791fa514cba6811d2e3d7b7
1rn19 <svaishnavi823@gmail.com>
2024-12-05 12:41:34 +0530

    learnt some of the important programs

---
+++ number-and-loops-JLC/src/com/class2/Lab16.java
@@ -0,0 +1,13 @@
+package com.class2;
+
+//  WAP to Print the digits of given number.
+
+public class Lab16 {
+
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
---
+++ number-and-loops-JLC/src/com/class3/Lab26.java
@@ -0,0 +1,40 @@
+package com.class3;
+
+// WAP to find whether given number is armStrong or not.
+
+public class Lab26 {
+
+	
+	public static void main(String[] args) {
+
+		int num=371;
+		int sum=0;
+		int numCopy=num;
+
+		while(num!=0) {
+			int temp=num%10;
+			sum=sum+cube(temp);
+			num=num/10;
+			
+			
+		}
+		if(sum==numCopy) {
+			System.out.println(numCopy+" is a Armstrong number");
+		}
+		else {
+
+			System.out.println(numCopy+" is not an Armstrong number");
+		}
+	}
+	
+	private static int cube(int num) {
+
+		int cube=0;
+		for(int i=1;i<=num;i++) {
+			cube=num*num*num;
+		}
+		return cube;
+		// return num*num*num;
+	}
+
+}
