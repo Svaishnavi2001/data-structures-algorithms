701e60dbe2766174453d14da8ea66d9997714a30
Vaishnavi <145185529+Svaishnavi2001@users.noreply.github.com>
2025-09-08 15:01:42 +0530

    Update Lab8.java

--- number-and-loops-JLC/src/com/number_and_loops/Lab8.java
+++ number-and-loops-JLC/src/com/number_and_loops/Lab8.java
@@ -6,28 +6,16 @@ public class Lab8 {
 
 	public static void main(String[] args) {
 
-		int n = 2; 
-//		int count = 0;
+		int n = 5;
 		
 		boolean primeFlag=primeOrNot(n);
 		System.out.println(primeFlag);
-        /*
-		 * for (int i = 2; i <= n-1; i++) {
-		 * 
-		 * if (n % i == 0) { count ++; break; }
-		 * 
-		 * }
-		 * 
-		 * if(count==0) { System.out.println(n + " is Prime: "); }else {
-		 * System.out.println(n + " is Not Prime: "); }
-		 */
-		
-
-	}
+   }
 
 	private static boolean primeOrNot(int n) {
 		
 		boolean primeFlag=false;
+		
 		for(int i=2;i<=n-1;i++) {
 			if(n%i==0) {
 				primeFlag=true;
